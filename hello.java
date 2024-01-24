4b39536d8d5d2067906c793d7c5972b88ad1744e
tekstacUser <tekstackuser@gmail.com>
2024-01-24 12:51:14 +0530

    update 3rd cmnt in Hello.java

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class Hello{
   public static void main(String [] args){
     System.out.println("Hello World Makkalehhhh...!!!");
     System.out.println("Welcome to Java program");
+    System.out.println("Welcome to Git project");
   }
 }
